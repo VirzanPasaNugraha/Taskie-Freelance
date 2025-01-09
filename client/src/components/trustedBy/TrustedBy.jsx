3d59e51b62b221ea5d65080c0ceadcb59d19e6f4
adriantea1 <virzanpasanugraha@gmail.com>
2025-01-09 17:23:13 +0700

    initial commit

---
+++ client/src/components/trustedBy/TrustedBy.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./TrustedBy.scss";
+
+const TrustedBy = () => {
+  return (
+    <div className="trustedBy">
+      <div className="container">
+        <h1>Tentang Taskie</h1>
+        <p>
+          Taskie adalah platform yang mempermudah kolaborasi antara freelancer
+          berbakat dan pemberi kerja. Kami memastikan setiap profil yang
+          tersedia adalah pilihan berkualitas, sehingga pencarian talenta atau
+          peluang menjadi lebih cepat, aman, dan andal.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default TrustedBy;
