fa9a989f0a291044f84c813cd7f7d955b02e7935
adriantea1 <virzanpasanugraha@gmail.com>
2025-01-09 18:31:05 +0700

    update order.controller.js

--- api/controllers/gig.controller.js
+++ api/controllers/gig.controller.js
@@ -60,8 +60,6 @@ export const getGig = async (req, res, next) => {
   }
 };
 
-// Fungsi untuk mendapatkan semua Gig berdasarkan filter
-// Fungsi untuk mendapatkan semua Gig berdasarkan filter
 export const getGigs = async (req, res, next) => {
   const q = req.query;
   const filters = {
@@ -77,18 +75,10 @@ export const getGigs = async (req, res, next) => {
   };
 
   try {
-    // Ambil gigs berdasarkan filter
+    // Ambil gigs berdasarkan filter tanpa menghitung sales
     const gigs = await Gig.find(filters).sort({ [q.sort]: -1 });
 
-    // Hitung penjualan hanya sekali
-    const updatedGigs = await Promise.all(
-      gigs.map(async (gig) => {
-        const sales = await Order.countDocuments({ gigId: gig._id });
-        return { ...gig._doc, sales };  // Gabungkan data gig dengan sales
-      })
-    );
-
-    res.status(200).json(updatedGigs);
+    res.status(200).json(gigs);
   } catch (err) {
     next(err);
   }
@@ -173,33 +163,7 @@ export const getTotalSales = async (req, res) => {
   }
 };
 
-// Fungsi untuk memperbarui penjualan
-export const updateSales = async (req, res) => {
-  const { gigId } = req.body;
 
-  // Mulai transaksi
-  const session = await mongoose.startSession();
-  session.startTransaction();
-
-  try {
-    // Ambil gig dari database dalam transaksi
-    const gig = await Gig.findById(gigId).session(session);
-
-    if (!gig) {
-      throw new Error("Gig not found!");
-    }
-
-    
-
-    res.status(200).json({ message: 'Sales updated', gig });
-  } catch (error) {
-    await session.abortTransaction();  // Gagal, rollback transaksi
-    console.error('Error updating sales:', error.message);
-    res.status(500).json({ message: 'Error updating sales' });
-  } finally {
-    session.endSession();  // Akhiri sesi transaksi
-  }
-};
 
 // Fungsi untuk mendapatkan jumlah Gig yang dimiliki berdasarkan userId
 export const getOwnedGigs = async (req, res, next) => {
